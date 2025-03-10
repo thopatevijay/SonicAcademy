0c381897328e77af16e4f13a5363ec93785f4290
thopatevijay <thopatevijay@gmail.com>
2025-03-10 15:46:53 +0530

    minor fixes in createLesson action

--- agent/src/sonic-agent-academy-plugin/index.ts
+++ agent/src/sonic-agent-academy-plugin/index.ts
@@ -1,5 +1,5 @@
 import type { Plugin } from "@elizaos/core";
-import { userLessonProvider } from "./providers/userLessonProvider.ts";
+// import { userLessonProvider } from "./providers/userLessonProvider.ts";
 import { createLesson } from "./actions/createLesson.ts";
 
 export const sonicAgentAcademyPlugin: Plugin = {
@@ -7,7 +7,8 @@ export const sonicAgentAcademyPlugin: Plugin = {
     description: "Sonic Agent Academy plugin",
     actions: [createLesson],
     clients: [],
-    providers: [userLessonProvider],
+    // comment out for now
+    // providers: [userLessonProvider],
 };
 
 export default sonicAgentAcademyPlugin;
